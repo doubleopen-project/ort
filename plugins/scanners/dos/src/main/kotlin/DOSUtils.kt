58f54fc70d53d90a498f82bad5d7c2619df40809
Sebastian Schuberth <sebastian@doubleopen.org>
2024-01-05 15:12:31 +0100

    feat(dos): Make PURLs take an optional provenance into account
    Extend the existing PURL conversion to take more than just a VCS path
    into account by using all VCS properties and also supporting source
    artifact properties.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- plugins/scanners/dos/src/main/kotlin/DOSUtils.kt
+++ plugins/scanners/dos/src/main/kotlin/DOSUtils.kt
@@ -9,13 +9,17 @@ package org.ossreviewtoolkit.plugins.scanners.dos
 import java.time.Duration
 import java.time.Instant
 
+import org.ossreviewtoolkit.model.KnownProvenance
 import org.ossreviewtoolkit.model.Package
+import org.ossreviewtoolkit.model.Provenance
+import org.ossreviewtoolkit.model.UnknownProvenance
+import org.ossreviewtoolkit.model.utils.toPurl
+import org.ossreviewtoolkit.model.utils.toPurlExtras
 
-internal fun Collection<Package>.getDosPurls(): List<String> =
-    map { pkg ->
-        pkg.purl.takeUnless { pkg.vcsProcessed.path.isNotEmpty() }
-            // Encode a path within the source code to the PURL.
-            ?: "${pkg.purl}#${pkg.vcsProcessed.path}"
+internal fun Collection<Package>.getDosPurls(provenance: Provenance = UnknownProvenance): List<String> =
+    when {
+        provenance is KnownProvenance -> map { it.id.toPurl(provenance.toPurlExtras()) }
+        else -> map { it.purl }
     }
 
 /**
