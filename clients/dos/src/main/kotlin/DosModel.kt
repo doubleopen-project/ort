781db8a1aae12ab8d5c503290cee4aba681dbb47
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2024-06-20 08:35:36 +0300

    refactor(dos): Edit job query parameters
    Add declared licenses of the packages to the request body in order to
    save this information about the package on DOS server side (so we can
    show this information in our Clearance UI).
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- clients/dos/src/main/kotlin/DosModel.kt
+++ clients/dos/src/main/kotlin/DosModel.kt
@@ -47,6 +47,15 @@ data class UploadUrlResponseBody(
 )
 
 @Serializable
+data class PackageInfo(
+    /** The purl for a package (includes the VCS information) */
+    val purl: String,
+    
+    /** The declared license for the package */
+    val declaredLicenseExpressionSPDX: String?
+)
+
+@Serializable
 data class ScanResultsRequestBody(
     /**
      * The list of purls to get scan results for. In case multiple purls are provided, it is assumed that they all
@@ -90,8 +99,8 @@ data class JobRequestBody(
     /** The key of the previously uploaded ZIP file to scan. */
     val zipFileKey: String,
 
-    /** The list of purls whose packages' source code is contained in the ZIP file. */
-    val purls: List<String>
+    /** The list of packages whose source code is contained in the ZIP file. */
+    val packages: List<PackageInfo>? = null
 )
 
 @Serializable
